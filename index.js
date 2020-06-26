c9085b5310639126c10696e676dc6dbcbd62b548
Joao Santos <joaomiguelopesantos@hotmail.com>
2020-06-26 11:22:16 +0200

    multer part complete

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const express = require("express");
 const { readAllImages } = require("./db");
 const app = express();
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 
 app.use(express.static("public"));
 
 app.get("/images", (req, res) => {
-    readAllImages().then((data) => {
-        return res.json(JSON.stringify(data.rows));
+    readAllImages().then(({ rows }) => {
+        return res.json(rows);
     });
+});
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    if (req.file) {
+        // send to s3
+        res.json({ success: true });
+    } else {
+        res.json({ success: false });
+    }
 });
 
 app.listen(8080, () => console.log("Listening...."));
--- public/js/script.js
+++ public/js/script.js
@@ -5,19 +5,47 @@
         // an object that we add any info to that is dynamic / we want to render onscreen
         data: {
             images: [],
+            title: "",
+            description: "",
+            file: null,
+            username: "",
         },
         mounted: function () {
             var self = this;
-            axios.get("/images").then((res) => {
-                self.images = JSON.parse(res.data);
-            });
+            axios
+                .get("/images")
+                .then(function (res) {
+                    self.images = res.data;
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
         },
         methods: {
             getImages: function () {
                 var self = this;
                 axios.get("/images").then((res) => {
-                    self.images = JSON.parse(res.data);
+                    self.images = res.data;
                 });
+            },
+            handleSubmit: function () {
+                var formData = new FormData();
+                formData.append("title", this.title);
+                formData.append("description", this.description);
+                formData.append("username", this.username);
+                formData.append("file", this.file);
+                axios
+                    .post("/upload", formData)
+                    .then(function (res) {
+                        console.log("res", res);
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            },
+            handleChange: function (e) {
+                this.file = e.target.files[0];
+                console.log("change", this, e.target.files[0]);
             },
         },
     });
